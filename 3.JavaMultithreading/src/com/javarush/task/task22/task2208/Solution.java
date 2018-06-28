13639b55ba3c580a7568a3958fa34a9318071237
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-06-28 21:51:43 +0200

    task2208 Solution.java

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Petkovic");
+        map.put("country", "Deutschland");
+        map.put("city", "Berlin");
+        map.put("age", null);
+        map.put("name1", "Becker");
+        map.put("name2", "Zimmermann");
+        map.put("country1", "Brandenburg");
+
+        System.out.println(getQuery(map));
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        if (params == null || params.isEmpty()) return result.toString();
+
+        for (Map.Entry<String, String> pair : params.entrySet()) {
+            if (pair.getKey() == null || pair.getValue() == null)
+                continue;
+
+            result.append(pair.getKey()).append(" = '").append(pair.getValue()).append("' and ");
+        }
+
+        if (result.length() > 5)
+            result.delete(result.length() - 5, result.length());
+
+        return result.toString();
+    }
+}
