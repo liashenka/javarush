d3b84d5a5b66cf6e5810f7dcfeecef987854d91f
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-07-17 06:16:38 +0200

    task2401 SelfInterfaceMarkerImpl.java

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void print() {
+        System.out.println("print1");
+    }
+
+    public void print2() {
+        System.out.println("print2");
+    }
+}
