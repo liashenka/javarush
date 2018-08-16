3ae385c0fa2f4943f8f537a1842d082d94dcac3e
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-08-16 21:32:03 +0200

    ConsoleHelper.java

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/chat/ConsoleHelper.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//вспомогательный класс, для чтения или записи в консоль
+public class ConsoleHelper {
+    private static String message;
+    private static int readInt;
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        while (true) {
+            try {
+                message = reader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+                try {
+                    message = reader.readLine();
+                } catch (IOException e1){
+                    
+                }
+            }
+        }
+        return message;
+    }
+
+    public static int readInt() throws IOException {
+        try {
+            readInt = Integer.parseInt(readString());
+        } catch (NumberFormatException e){
+            System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            readInt = Integer.parseInt(readString());
+        }
+        return readInt;
+    }
+}
