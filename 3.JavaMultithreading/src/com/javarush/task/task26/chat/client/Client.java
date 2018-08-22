f28aca8c3b58e73e732363b9888bd23988a9e433
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-08-22 20:19:10 +0200

    Client.java

--- 3.JavaMultithreading/src/com/javarush/task/task26/chat/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task26/chat/client/Client.java
@@ -8,6 +8,7 @@ import com.javarush.task.task30.task3008.MessageType;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class Client {
     protected Connection connection;
@@ -19,7 +20,7 @@ public class Client {
     }
 
     public void run() throws IOException {
-        boolean loop = true;
+        // boolean loop = true;
         SocketThread socketThread = getSocketThread();
         socketThread.setDaemon(true);   //при выходе из программы вспомогательный поток прервется автоматически
         socketThread.start();
@@ -85,6 +86,78 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
 
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage(userName + " joined the chat.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage(userName + " left the chat.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            Message message;
+
+            while (true) {
+                message = connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST) {
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                } else {
+                    if (message.getType() == MessageType.NAME_ACCEPTED) {
+                        notifyConnectionStatusChanged(true);
+                        break;
+                    } else {
+                        throw new IOException("Unexpected MessageType");
+                    }
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            Message message;
+            while (true) {
+                message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                } else {
+                    if (message.getType() == MessageType.USER_ADDED) {
+                        informAboutAddingNewUser(message.getData());
+                    } else {
+                        if (message.getType() == MessageType.USER_REMOVED) {
+                            informAboutDeletingNewUser(message.getData());
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+            throw new IOException("Unexpected MessageType");
+        }
+
+        public void run(){
+
+            try {
+                String ipAdress = getServerAddress();
+                int serverPort = getServerPort();
+                Socket socket = new Socket(ipAdress, serverPort);
+                connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException e) {
+                notifyConnectionStatusChanged(false);
+            } catch (ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+        }
     }
 }
