c6afdc13b2ac97ffbfbea9dc62b412303b4f8db6
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-08-23 23:00:07 +0200

    Solution.java

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2702/Solution.java
@@ -9,7 +9,8 @@ public class Solution {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j > i) {
-                    System.out.println();
+                   System.out.println();
+                   continue label;
                 }
                 System.out.print("S");
             }
