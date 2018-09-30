4ddea0332b2cfb2798f2d22c30fb142c15d005a7
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-09-30 18:04:23 +0200

    Add files via upload

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/restaurant/Tablet.java
@@ -0,0 +1,79 @@
+package com.javarush.task.task27.task2712;
+
+
+import com.javarush.task.task27.task2712.ad.AdvertisementManager;
+import com.javarush.task.task27.task2712.ad.NoVideoAvailableException;
+import com.javarush.task.task27.task2712.kitchen.Order;
+import com.javarush.task.task27.task2712.kitchen.TestOrder;
+
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Tablet {
+    final int number;
+    private static Logger logger = Logger.getLogger(Tablet.class.getName());
+    private LinkedBlockingQueue<Order> queue = new LinkedBlockingQueue<>();
+
+    public Tablet(int number) {
+        this.number = number;
+    }
+
+    public void setQueue(LinkedBlockingQueue<Order> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public String toString() {
+        return "Tablet{" +
+                "number=" + number +
+                '}';
+    }
+
+    public Order createOrder() {
+        Order order = null;
+        try {
+            order = new Order(this);
+            processOrder(order);
+            return order;
+        } catch (NoVideoAvailableException e) {
+            logger.log(Level.INFO, "No video is available for the order " + order);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
+        return null;
+    }
+
+
+    public void createTestOrder() {
+        TestOrder order = null;
+        try {
+            order = new TestOrder(this);
+            processOrder(order);
+
+        } catch (NoVideoAvailableException e) {
+            logger.log(Level.INFO, "No video is available for the order " + order);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
+
+    }
+
+    private void processOrder(Order order) {
+        if (!order.isEmpty()) {
+            ConsoleHelper.writeMessage(order.toString());
+            //setChanged();
+            //this.notifyObservers(order);
+
+           try {
+                queue.put(order);
+            } catch (InterruptedException e) {
+
+            }
+
+
+            new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
+        }
+    }
+}
