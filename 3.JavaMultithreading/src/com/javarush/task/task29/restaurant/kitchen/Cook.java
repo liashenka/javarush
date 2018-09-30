fcff89640268fae46c108f77e91eb3a91d570c82
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-09-30 18:07:06 +0200

    Cook.java

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/restaurant/kitchen/Cook.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import com.javarush.task.task27.task2712.ConsoleHelper;
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
+import com.javarush.task.task27.task2712.statistic.event.CookedOrderEventDataRow;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Cook extends Observable implements Runnable{
+    private String name;
+    private boolean busy;
+    private LinkedBlockingQueue<Order> queue = new LinkedBlockingQueue<>();
+
+    public boolean isBusy() {
+        return busy;
+    }
+
+    public void setQueue(LinkedBlockingQueue<Order> queue) {
+        this.queue = queue;
+    }
+
+    public Cook(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+
+    public void startCookingOrder(Order order) throws InterruptedException {
+
+        busy = true;
+
+        ConsoleHelper.writeMessage("Start cooking - " + order + ", cooking time " + order.getTotalCookingTime() + "min");
+        StatisticManager.getInstance().register(new CookedOrderEventDataRow(order.tablet.toString(), this.toString(), order.getTotalCookingTime() * 60, order.getDishes()));
+        Thread.sleep(order.getTotalCookingTime() * 10);
+        setChanged();
+        notifyObservers(order);
+
+        busy = false;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                startCookingOrder(queue.take());
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+}
