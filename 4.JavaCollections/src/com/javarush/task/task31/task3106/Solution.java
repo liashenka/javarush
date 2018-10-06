ca705697b4418e1c69b3433e170ceb405e346027
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-10-06 19:53:05 +0200

    Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task31.task3106;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+/*
+Разархивируем файл
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String resultFileName = args[0];
+        int filePartCount = args.length - 1;
+        String[] fileNamePart = new String[filePartCount];
+        for (int i = 0; i < filePartCount; i++) {
+            fileNamePart[i] = args[i + 1];
+        }
+        Arrays.sort(fileNamePart);
+
+        List<FileInputStream> fisList = new ArrayList<>();
+        for (int i = 0; i < filePartCount; i++) {
+            fisList.add(new FileInputStream(fileNamePart[i]));
+        }
+
+        SequenceInputStream sequenceInputStream = new SequenceInputStream(Collections.enumeration(fisList));
+        ZipInputStream zipInputStream = new ZipInputStream(sequenceInputStream);
+        FileOutputStream fileOutputStream = new FileOutputStream(resultFileName);
+        byte[] buffer = new byte[1024 * 1024];
+
+        while (zipInputStream.getNextEntry() != null){
+            int count;
+            while ((count = zipInputStream.read(buffer)) != -1){
+                fileOutputStream.write(buffer, 0, count);
+            }
+        }
+
+        sequenceInputStream.close();
+        zipInputStream.close();
+        fileOutputStream.close();
+
+    }
+}
