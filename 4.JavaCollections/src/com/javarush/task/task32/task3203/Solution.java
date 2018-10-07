b7218caa59251c4db073c89328e9d153f414d234
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-10-07 20:36:38 +0200

    Solution.java

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3203/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task32.task3203;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/*
+Пишем стек-трейс
+*/
+public class Solution {
+    public static void main(String[] args) {
+        String text = getStackTrace(new IndexOutOfBoundsException("fff"));
+        System.out.println(text);
+    }
+
+    public static String getStackTrace(Throwable throwable) {
+        StringWriter writer = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(writer));
+
+        return writer.toString();
+    }
+}
+
+/*
+public void printStackTrace(PrintWriter s)
+
+Prints this throwable and its backtrace to the specified print writer.
+
+Parameters:
+    s - PrintWriter to use for output
+
+    PrintWriter(Writer out)
+Creates a new PrintWriter, without automatic line flushing.
+    */
