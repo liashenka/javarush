856d90a6b5d4c83dfdd4c48a34d1f0bbf2280e67
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-11-05 21:37:14 +0100

    Add files via upload

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task35.task3512;
+
+public class Generator<T> {
+    Class<T> eventClass;
+
+    public Generator(Class<T> eventClass) {
+        this.eventClass = eventClass;
+    }
+
+    T newInstance() throws IllegalAccessException, InstantiationException {
+        return (T) eventClass.newInstance();
+    }
+}
