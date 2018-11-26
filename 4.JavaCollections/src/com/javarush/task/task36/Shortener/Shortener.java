25693ef77516beb60e26f957263bef7b1175e1c3
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-11-26 21:24:46 +0100

    Add files via upload

---
+++ 4.JavaCollections/src/com/javarush/task/task36/Shortener/Shortener.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task33.task3310;
+
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+public class Shortener {
+    private Long lastId = 0L; //отвечает за последнее значение идентификатора, которое было использовано при добавлении новой строки в хранилище.
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
+    }
+
+    public synchronized Long getId(String string) {
+        //будет возвращать идентификатор id для заданной строки.
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        } else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+    }
+
+    public synchronized String getString(Long id) {
+        //будет возвращать строку для заданного идентификатора или null, если передан неверный идентификатор.
+            return storageStrategy.getValue(id);
+    }
+}
