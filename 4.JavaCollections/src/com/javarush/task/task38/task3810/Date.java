9e98ed6d1cb4669af73e6e3480541af2f2aaf1fc
Dmitry <37516114+liashenka@users.noreply.github.com>
2018-12-27 00:08:28 +0300

    Add files via upload

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Date.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Date {
+    //напиши свой код
+    public int year ();
+    public int month ();
+    public int day ();
+    public int hour ();
+    public int minute ();
+    public int second ();
+}
